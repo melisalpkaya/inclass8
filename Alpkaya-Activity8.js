24274e8740a2d1ae67d0d58e38279f865efbfffc
Melis Alpkaya <melisalpkaya200011@gmail.com>
2022-05-12 15:34:05 +0300

    Add files via upload

---
+++ Alpkaya-Activity8.js
@@ -0,0 +1,18 @@
+
+
+    var languages = [
+        "ActionScript", 
+        "AppleScript", 
+        "Asp",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python" 
+      ];
+
+    $( "#language" ).autocomplete({
+        source: languages 
+    });
+    $( "#birthday" ).datepicker();
+
